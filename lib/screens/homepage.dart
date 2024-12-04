a3f3c41d23173d58c98b8cd29cdb89075969a7aa
Sami9837 <samisaifi043@gmail.com>
2024-12-04 13:31:23 +0530

    giving initialisation for musicapi and player

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,17 +1,90 @@
 import 'package:flutter/material.dart';
-import 'package:music_apk/widgets/bottom_navbar.dart';
+import '../api/music_api.dart';
+import '../widgets/floating_music_player.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  List<dynamic> songs = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchSongs();
+  }
+
+  Future<void> fetchSongs() async {
+    final fetchedSongs = await MusicApi().fetchSongs();
+    setState(() {
+      songs = fetchedSongs;
+    });
+  }
+
+  void playSong(song) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MusicPlayerPage(song: song),
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var _pages;
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Row(
+          children: [
+            Image.asset('assets/logo.png', height: 30),
+            const SizedBox(width: 8),
+            const Text('Raag',
+                style: TextStyle(color: Color(0xFFE9A6A6), fontSize: 22)),
+          ],
+        ),
       ),
-      body: const Center(
-        child: Text('Welcome to Raag!', style: TextStyle(fontSize: 20)),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: const Color(0xFFE9A6A6),
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_music),
+            label: 'Library',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
       ),
     );
   }
