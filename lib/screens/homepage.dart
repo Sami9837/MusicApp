8547830e996f29c0bf94ed0ec0bbedcbd15f51fb
Sami9837 <samisaifi043@gmail.com>
2024-12-05 13:55:43 +0530

    initialize library, profile and search page

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -59,27 +59,29 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: Center(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset('assets/logo.png', height: 25),
-              const SizedBox(width: 8),
-              const Text(
-                'Raag',
-                style: TextStyle(
-                    color: Color(0xFFF39F59),
-                    fontSize: 24,
-                    fontFamily: 'KumbhSans',
-                    fontWeight: FontWeight.bold),
+      appBar: _selectedIndex == 0
+          ? AppBar(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              title: Center(
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Image.asset('assets/logo.png', height: 25),
+                    const SizedBox(width: 8),
+                    const Text(
+                      'Raag',
+                      style: TextStyle(
+                          color: Color(0xFFF39F59),
+                          fontSize: 24,
+                          fontFamily: 'KumbhSans',
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ),
-        ),
-      ),
+            )
+          : null,
       body: _pages()[_selectedIndex],
       bottomNavigationBar: BottomNavBar(
         selectedIndex: _selectedIndex,
